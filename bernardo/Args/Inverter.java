d0da91a5d2a953361470790797cfcba68d4fa7e9
BeSimonetti <151688438+BeSimonetti@users.noreply.github.com>
2024-02-23 00:45:31 +0000

    22_02.2

--- bernardo/Args/Inverter.java
+++ bernardo/Args/Inverter.java
@@ -1,7 +1,13 @@
 public class Inverter{
     public static void main(String[] args){
-        String nome = args[0];
-        String sobrenome = args[1];
-        System.out.println(sobrenome + nome );
+        for(int i=args.length-1; i>=0; i--){
+            for(int x=args[i].length()-1; x>=0; x--){
+                System.out.print(args[0].charAt(x));
+            }
+            if(i>0){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
     }
 }
